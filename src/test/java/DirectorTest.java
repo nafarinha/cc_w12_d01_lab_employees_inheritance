50c8bb0f4d3799922dd3bd28534ca76cedb2a28d
Nuno Farinha <nadfarinha@gmail.com>
2019-04-01 16:57:05 +0100

    Test inherited functions from employee on director class

--- src/test/java/DirectorTest.java
+++ src/test/java/DirectorTest.java
@@ -2,6 +2,8 @@ import management.Director;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class DirectorTest {
 
     Director director1;
@@ -10,4 +12,36 @@ public class DirectorTest {
     public void before() {
         director1 = new Director("Jonathan Davidson", "QR123456X", 10000, "Admin", 1000000);
     }
+
+    @Test
+    public void hasName() {
+        assertEquals("Jonathan Davidson", director1.getName());
+    }
+
+    @Test
+    public void hasNINumber() {
+        assertEquals("QR123456X", director1.getNINumber());
+    }
+
+    @Test
+    public void hasSalary() {
+        assertEquals(10000, director1.getSalary(), 0);
+    }
+
+
+//    @Test
+//    public void hasDeptName() {
+//        assertEquals("Admin", director1.getDeptName());
+//    }
+//
+//    @Test
+//    public void canRaiseSalary() {
+//        manager1.raiseSalary(2075);
+//        assertEquals(12075, director1.getSalary(), 0);
+//    }
+//
+//    @Test
+//    public void canPayBonus() {
+//        assertEquals((100), director1.payBonus(), 0.01);
+//    }
 }
