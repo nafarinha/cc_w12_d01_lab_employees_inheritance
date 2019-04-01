a88654f6cd75ad659eb64b037579adf222d6df90
Nuno Farinha <nadfarinha@gmail.com>
2019-04-01 15:36:29 +0100

    Create get dept name function on manager class

--- src/main/java/Management/Manager.java
+++ src/main/java/Management/Manager.java
@@ -3,4 +3,18 @@ package Management;
 import Staff.Employee;
 
 public class Manager extends Employee {
+
+    private String deptName;
+
+
+    public Manager(String name, String nINumber, double salary, String deptName) {
+        super(name, nINumber, salary);
+        this.deptName = deptName;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+
 }
---
+++ src/test/java/ManagerTest.java
@@ -0,0 +1,22 @@
+import Management.Manager;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ManagerTest {
+
+    Manager manager1;
+
+    @Before
+    public void before() {
+        manager1 = new Manager("Joe Doe", "QQ123456C", 5000, "HR");
+    }
+
+    @Test
+    public void hasDeptName() {
+        assertEquals("HR", manager1.getDeptName());
+    }
+
+
+}
